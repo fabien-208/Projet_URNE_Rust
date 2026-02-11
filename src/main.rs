3e4e9efe1018d6440880d2fd77d0c665f02db739
TheSurvivor66 <fafabien.m@gmail.com>
2026-02-11 17:00:21 +0100

    Add parser module and integrate it in main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod vote;
 mod types;
+mod parser;
 
 use vote::*;
+use types::*;
+use parser::*;
 
 fn main() {
     println!("Hello, world!");
+    let elec: Election = parser("Data_very_long.txt");
 }
