add99cc39153f332b635014e7bd98d56342e55c7
JRLapierre <jeanremi.lapierre@gmail.com>
2026-02-11 17:17:53 +0100

    algorithme plurality (theoriquement)

--- src/vote/plurality.rs
+++ src/vote/plurality.rs
@@ -1,13 +1,24 @@
-use crate::{VotingAlgorithm};
+use crate::{VotingAlgorithm, types::{CandidateId, VoteResult}};
 
 pub struct Plurality;
 
 impl VotingAlgorithm for Plurality {
     fn name(&self) -> String {
-        todo!()
+        return "Plurality".to_string();
     }
 
     fn compute(&self, election: &crate::types::Election) -> crate::types::VoteResult {
-        todo!()
+        //creer une liste de la taille de la liste des candidats
+        let mut scores = vec![0; election.candidates.len()];
+        //prendre les premiers éléments de chaque liste, et regarder a quel candidat il correspondent
+        for v in election.ballots.iter() {
+            scores[v.ranking[0]]+=1;
+        }
+        //liste des candidats
+        let mut ranking: Vec<CandidateId> = (0..scores.len()).collect();
+        //tri de la liste des candidats
+        ranking.sort_by_key(|&i| std::cmp::Reverse(scores[i]));
+
+        return VoteResult {ranking};
     }
 }
